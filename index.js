a9e2b685f4cca947fc7328c75a95877e7560fc8e
jaswant2111058 <jaswant2111058@akgec.ac.in>
2023-01-07 13:44:32 +0530

    first_project

---
+++ index.js
@@ -0,0 +1,24 @@
+function add()
+{
+ const num1 = document.getElementById("num1").value
+ const num2 = document.getElementById("num2").value
+ const num3 = num1+num2;
+ document.getElementById("num3").value = num3;
+
+}
+function sub()
+{
+    const num1 = document.getElementById("num1").value
+    const num2 = document.getElementById("num2").value
+    const num3 = num1-num2;
+    document.getElementById("num3").value = num3;
+   
+}
+function multi()
+{
+    const num1 = document.getElementById("num1").value
+    const num2 = document.getElementById("num2").value
+    const num3 = num1*num2;
+    document.getElementById("num3").value = num3;
+   
+}
